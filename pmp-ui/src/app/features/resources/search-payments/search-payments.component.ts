4280888893b2caf593fea34675de68d482b557c4
Soumya Raj <soumya.raj@technoforte.co.in>
2026-02-12 12:37:25 +0530

    edit search-payment component

--- pmp-ui/src/app/features/resources/search-payments/search-payments.component.ts
+++ pmp-ui/src/app/features/resources/search-payments/search-payments.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataStorageService } from 'src/app/core/services/data-storage.service';
 import { HeaderService } from 'src/app/core/services/header.service';
 import { AuditService } from 'src/app/core/services/audit.service';
@@ -17,6 +17,10 @@ export class SearchPaymentsComponent implements OnInit {
   isLoading: boolean = false;
   searchResults: any[] = [];
   showResults: boolean = false;
+  displayedColumns: any[] = [];
+  paginatorOptions: any = {};
+  totalRecords: number = 0;
+  partners: any[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -26,10 +30,13 @@ export class SearchPaymentsComponent implements OnInit {
     public translateService: TranslateService
   ) {
     this.searchForm = this.formBuilder.group({
-      dateRange: [''],
-      partner: [''],
-      prn: [''],
-      category: ['']
+      transactionId: [''],
+      partnerId: [''],
+      entryType: [''],
+      amount: [''],
+      sourceSystem: [''],
+      description: [''],
+      crDtimes: ['']
     });
   }
 
@@ -39,26 +46,57 @@ export class SearchPaymentsComponent implements OnInit {
       .subscribe(response => {
         this.labels = response['payments'] || {};
       });
+    
+    this.loadPaymentSearchConfig();
+    this.loadPartners();
+  }
+
+  loadPaymentSearchConfig() {
+    this.dataService.getSpecFileForMasterDataEntity('payments-search').subscribe(
+      (response: any) => {
+        if (response && response.columnsToDisplay) {
+          this.displayedColumns = response.columnsToDisplay.filter(
+            values => values.showInListView === 'true'
+          );
+          this.paginatorOptions = response.paginator;
+        }
+      },
+      (error: any) => {
+        console.error('Error loading payment search config:', error);
+      }
+    );
+  }
+
+  loadPartners() {
+    this.dataService.getPartners().subscribe(
+      (response: any) => {
+        if (response && response.response && response.response.partners) {
+          this.partners = response.response.partners;
+        }
+      },
+      (error: any) => {
+        console.error('Error loading partners:', error);
+        this.partners = [];
+      }
+    );
   }
 
   searchPayments() {
     this.isLoading = true;
-    const partnerId = this.headerService.getUsername();
-    const filterCriteria = {
-      ...this.searchForm.value,
-      partnerId
-    };
+    const filterCriteria = this.searchForm.value;
 
     const request = new RequestModel(
-      '',
-      null,
+      'mosip.registration.partner.payment.search.1.0',
+      '1.0',
       filterCriteria
     );
 
     this.dataService.searchPayments(request).subscribe(
       (response: any) => {
         if (response && response.response) {
-          this.searchResults = response.response.payments || [];
+          this.searchResults = response.response.data || [];
+          this.totalRecords = response.response.totalRecord || 0;
+          this.paginatorOptions.totalEntries = this.totalRecords;
           this.showResults = true;
         }
         this.isLoading = false;
